08af42ca9f7cad7af75a214567a63051f374f25a
ramjak <rama.jakaria@gmail.com>
2022-07-21 18:54:44 +0700

    feat: base RepoPage

--- src/modules/repo/RepoPage.tsx
+++ src/modules/repo/RepoPage.tsx
@@ -1,10 +1,22 @@
 import { FC } from "react";
+import { faBook } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import classes from "./RepoPage.module.scss";
+import { IRouteParams, useParams } from "../../routes";
 
 interface IRepoPage {}
 
 const RepoPage: FC<IRepoPage> = (props) => {
-  return <div>Repo</div>;
+  const { repo } = useParams<IRouteParams["viewRepo"]>();
+
+  return (
+    <div>
+      <div className={classes.shortProfile}>
+        <FontAwesomeIcon icon={faBook} className={classes.searchIcon} />
+        <div>{repo}</div>
+      </div>
+    </div>
+  );
 };
 
 export default RepoPage;
