b081be2da13ea1951e2cd663b4a6a712fb7bc0c4
ramjak <rama.jakaria@gmail.com>
2022-07-21 17:48:04 +0700

    feat: base HomePage

--- src/modules/home/HomePage.tsx
+++ src/modules/home/HomePage.tsx
@@ -1,10 +1,43 @@
 import { FC } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import classes from "./HomePage.module.scss";
+import ROUTES, { Link } from "../../routes";
 
 interface IHomePage {}
 
 const HomePage: FC<IHomePage> = (props) => {
-  return <div>Home</div>;
+  return (
+    <div className={classes.homePage}>
+      <div className={classes.searchBar}>
+        <FontAwesomeIcon
+          icon={faMagnifyingGlass}
+          className={classes.searchIcon}
+        />
+        <input
+          type="text"
+          className={classes.input}
+          placeholder="Search user"
+        />
+      </div>
+      <div className={classes.result}>
+        <div className={classes.listUser}>
+          {Array(20)
+            .fill(true)
+            .map(() => (
+              <Link route={ROUTES.viewUser} params={["test"]}>
+                <div className={classes.user}>
+                  <img src="https://via.placeholder.com/32" alt="" />
+                  <div>name</div>
+                  <div className={classes.username}>username</div>
+                </div>
+              </Link>
+            ))}
+        </div>
+        {/*<div className={classes.higlight}>highlight</div>*/}
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
